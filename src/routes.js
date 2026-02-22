d06b31e45d4aaeb6f2bb3498f9787000864214a6
Hansen <hansenwijaya2004@gmail.com>
2026-02-22 20:41:09 +0700

    Initial backend structure

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// Import module routes here
+// const authRoutes = require('./modules/auth/auth.routes');
+// const userRoutes = require('./modules/users/user.routes');
+
+// Register routes
+// router.use('/auth', authRoutes);
+// router.use('/users', userRoutes);
+
+module.exports = router;
