b2d6d6e65c05ec78ab345b13646b23b87811f766
Hansen <hansenwijaya2004@gmail.com>
2026-06-08 23:49:55 +0700

    split cron and worker

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = require('./src/app');
 const { connectDB } = require('./src/config/db');
 const http = require('http');
 const initializeSocket = require('./src/config/socket');
-const startCronJobs = require('./src/config/cron');
 
 const server = http.createServer(app);
 const io = initializeSocket(server);
@@ -11,11 +10,13 @@ app.set('io', io);
 
 async function bootstrap() {
   await connectDB();
-  startCronJobs();
 
   server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 }
 
-bootstrap();
+bootstrap().catch((error) => {
+  console.error('Failed to start web server:', error);
+  process.exit(1);
+});
